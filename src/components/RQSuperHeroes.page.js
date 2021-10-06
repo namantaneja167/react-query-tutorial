67e1cb285e41b76daa2d8d8c117ab1764ea68fd3
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2021-10-07 01:20:10 +0530

    Donw till stale time

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import { useQuery} from "react-query"
+import axios from 'axios'
+
+const fetchSuperHeros = () => {
+    return axios.get('http://localhost:4000/superheroes')
+}
 
 const RQSuperHeroesPage = () => {
+    const {isLoading,data,isError,error} = useQuery(
+        'super-heroes',
+        fetchSuperHeros,
+        {
+            cacheTime:5000,
+            staleTime:3000,
+        }
+    );
+
+    if(isLoading){
+        return <h1>Loading...</h1>
+    }
+    
+    if(isError){
+        return <h1>{error}</h1>
+    }
+
     return (
-        <div>
-            RQSuperHerosPage
-        </div>
+        <>
+            <h2>RQ Super Heroes Page</h2>
+            {data?.data.map((hero) => {
+                return <div key={hero.name}>{hero.name}</div> 
+            })}
+        </>
     )
 }
 
