78ce631df20ad479f6f4bad917a66446f826245e
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2021-10-07 00:20:53 +0530

    React Query Setup Done

---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RQSuperHeroesPage = () => {
+    return (
+        <div>
+            RQSuperHerosPage
+        </div>
+    )
+}
+
+export default RQSuperHeroesPage
